e12fda4aba41c205ea2903ad2023be2f31f70afb
JonathanLop1 <147114171+JonathanLop1@users.noreply.github.com>
2024-05-25 14:00:26 -0500

    Add files via upload

---
+++ crear usuario/index.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const correo = document.getElementById("correo");
+const cedula = document.getElementById("cedula");
+const numero = document.getElementById("numero");
+const agregar = document.getElementById("agregar");
+const limpiar = document.getElementById("limpiar");
